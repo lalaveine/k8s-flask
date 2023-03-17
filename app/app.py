ef415554fa6e5f34e50278800d98d1ab9469e0cd
Evgenii Matveev <mfb.eugene@gmail.com>
2023-03-17 08:18:59 +0300

    first commit

---
+++ app/app.py
@@ -0,0 +1,20 @@
+from flask import Flask, jsonify
+import time
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def root():
+    return jsonify({"App Status" : "Running"})
+
+@app.route("/getTime")
+def getTime():
+    seconds = time.time()
+    local_time = time.localtime(seconds)
+    return jsonify({"Year" : local_time.tm_year,"Seconds" : seconds, "Local Time" : local_time})
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
